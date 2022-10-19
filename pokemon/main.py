9fcabb50099999192ebddfa5296acb2830a8ce2e
alistairgrenyer <alistair.grenyer@capgemini.com>
2022-10-19 10:52:48 +0100

    added requests

--- pokemon/main.py
+++ pokemon/main.py
@@ -4,43 +4,48 @@ import pokemonDatabase
 
 app = Flask(__name__)
 
+
 # Creates route (url)
 @app.route("/")
 def main():
-  return indexPage(None)
+    return indexPage(None)
+
 
 def indexPage(downloadSuccess):
     # Get list of pokemon from database.
-  # conn = pokemonDatabase.databaseConnect()
-  # pokemonDictList = pokemonDatabase.findAllPokemon(conn)
-  # conn.close()
+    # conn = pokemonDatabase.databaseConnect()
+    # pokemonDictList = pokemonDatabase.findAllPokemon(conn)
+    # conn.close()
     #
-  pokemonList = []
-  return render_template("index.html", pokemon=pokemonList, downloadSuccess=downloadSuccess)
+    pokemonList = []
+    return render_template("index.html", pokemon=pokemonList, downloadSuccess=downloadSuccess)
+
 
 @app.route("/downloadPokemon")
 def downloadPokemonData():
     # true/false
-  downloadSuccess = downloadDB.downloadPokemonData()
-  return indexPage(downloadSuccess)
+    downloadSuccess = downloadDB.downloadPokemonData()
+    return indexPage(downloadSuccess)
+
 
 @app.route("/pokemonCard")
 def produceCard():
-  args=request.args
-    
-  # conn = pokemonDatabase.databaseConnect()
-  # pokemonData = pokemonDatabase.findPokemonFromName(conn, args)
-  # conn.close()
-  pokemonData = {"Name": "", "Artwork": ""}
-  pokemonName = pokemonData["Name"]
-  pokemonArtwork = pokemonData["Artwork"]
-  # pokemonAttack = pokemonData["Attack"]
-  # pokemonDefence = pokemonData["Defence"]
-  # pokemonType1 = pokemonData["Type1"]
-  # pokemonType2 = pokemonData["Type2"]
+    args = request.args
+
+    # conn = pokemonDatabase.databaseConnect()
+    # pokemonData = pokemonDatabase.findPokemonFromName(conn, args)
+    # conn.close()
+    pokemonData = {"Name": "", "Artwork": ""}
+    pokemonName = pokemonData["Name"]
+    pokemonArtwork = pokemonData["Artwork"]
+    # pokemonAttack = pokemonData["Attack"]
+    # pokemonDefence = pokemonData["Defence"]
+    # pokemonType1 = pokemonData["Type1"]
+    # pokemonType2 = pokemonData["Type2"]
     # 
-  return render_template("pokemonCard.html", pokemonCardName=pokemonName, pokemonArtwork=pokemonArtwork,
-  )
+    return render_template("pokemonCard.html", pokemonCardName=pokemonName, pokemonArtwork=pokemonArtwork,
+                           )
+
 
 if __name__ == '__main__':
     app.run()
