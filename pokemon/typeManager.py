5e9f50ada1c61955d1d2ceca8380de415f333945
alistairgrenyer <alistair.grenyer@capgemini.com>
2022-10-24 15:20:35 +0100

    first commit

--- pokemon/typeManager.py
+++ pokemon/typeManager.py
@@ -1,11 +1,27 @@
 from pokemon.pokemonCard import Pokemon
+import logs
+import requests
+import json
 
 
 class TypesManager:
-    # Singleton
+    _instance = None
+
+    def __init__(self):
+        self.typesManagerLogs = logs.Logger()
+        self.URL = "https://pokeapi.co/api/v2/type/"
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(TypesManager, cls).__new__(cls)
+        return cls._instance
 
     def downloadAndStore(self):
-        pass
+        try:
+            content = requests.get(self.URL)
+            parsedJson = json.loads(content.text)
+        except Exception as e:
+            self.typesManagerLogs.logger.error(e)
 
     def getDamagerMultiplier(self, attackType: str, defenderPokemon: Pokemon) -> float:
         return 1.0
