626fe4b043304dfa8beda033d6ee36c968c8791f
Isaac Hulatt <isaac.hulatt@capgemini.com>
2022-10-26 17:23:40 +0100

    Fixed submit button bug

--- pokemon/static/allJavascriptFiles.js
+++
@@ -1,8 +0,0 @@
-
-function updateLists() {
-    fetch('/pokeList')
-        .then(data => data.text())
-        .then(data => {
-            element = document.getElementById("pokeList");
-            element.innerHTML = data
-        });
